81b28536bc7c7af785d2182b6c48739c75574115
davide.frigoli <davide.frigoli@gmail.com>
2025-11-25 15:02:45 +0000

    Breve descrizione delle modifiche

---
+++ .idx/dev.nix
@@ -0,0 +1,53 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20  # <--- 1. ABBIAMO ATTIVATO NODE.JS
+    # pkgs.python311
+    # pkgs.python311Packages.pip
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          # <--- 2. CONFIGURAZIONE DEL SERVER WEB
+          # Questo comando scarica al volo "http-server" e lo avvia sulla porta corretta
+          command = ["npx" "http-server" "." "--port" "$PORT"];
+          manager = "web";
+          env = {
+            # Environment variables to set for your server
+            PORT = "$PORT";
+          };
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
